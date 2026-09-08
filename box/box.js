2daf755ca22a7d453290fc42e69d208c7b603c18
Aether <140750964+this-progammer@users.noreply.github.com>
2026-09-08 12:00:02 -0500

    Update box.js

--- box/box.js
+++ box/box.js
@@ -33,4 +33,21 @@ class Box {
     return this.name;
   }
 
+  /*createBox()*/
+  createBox( w, h, name ) {
+      const box = new Box( w, h, name );
+        if( name == null ) {
+            box.getName() = box.defBoxName;
+        }
+          for( let i = 0; i <= 0; i++ ) {
+              box.getBox();
+          }
+            if( box != null ) {
+                for( let j = 0; j <= 0; j++ ) {
+                    console.log("New Box Created\n");
+                }
+            }
+    return box;
+  }
+
 };
